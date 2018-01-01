b9ab89ec0a878a78e72d8fb6276eee679fbd5d10
Julie Groth <groth.julie@gmail.com>
2018-01-01 14:40:24 -0800

    pseudocode added to js file

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,13 @@
-console.log("test");
+//From start pages, user clicks button to begin game
+//First question is displayed:
+    //Countdown at top to show how many seconds remain
+    //Multiple choice options available
+//Options are highlighted as user hover overs the option
+//User selects one option
+//Correct answer is shown. 
+    //User is told if they answered correctly or not
+    //# correct or incorrect answers increases
+//After certain amount of time, next question is displayed. 
+    //again, countdown is shown and multiple options are shown until user makes a selection
+//Once the last question is answered, shows # correct, # incorrect, and # unanswered. 
+    //Button to start over without refreshing the page
