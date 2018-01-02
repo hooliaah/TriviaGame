a26b692f323727311e674ee4ed847b084adedddd
Julie Groth <groth.julie@gmail.com>
2018-01-01 22:07:27 -0800

    added questionTimer function, hover styling, variables for correct, incorrect, and unanswered

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,13 @@
-//From start page, user clicks button to begin game
-//First question is displayed:
-    //Countdown at top to show how many seconds remain
-    //Multiple choice options available
-//Options are highlighted as user hover overs the option
-//User selects one option
-//Correct answer is shown. 
-    //User is told if they answered correctly or not
-    //# correct or incorrect answers increases
 //After certain amount of time, next question is displayed. 
     //again, countdown is shown and multiple options are shown until user makes a selection
 //Once the last question is answered, shows # correct, # incorrect, and # unanswered. 
     //Button to start over without refreshing the page
 $(document).ready(function(){
+
+var time = 30;
+var numberCorrect = 0;
+var numberIncorrect = 0;
+var numberUnanswered = 0;
 
 var options = {
     question1: {
@@ -21,17 +17,29 @@ var options = {
         
 };
 
+//From start page, user clicks button to begin game
 $("#start-game").on("click", function(){
     console.log(options.question1.question);
+    $("time-remaining").text(time); //why doesn't this show 30?!?
     showQuestion();
     showAnswers();
+    questionTimer();
 });
 
+//First question displayed
 function showQuestion() {
     $("#start-game").hide();
     $("#question").text(options.question1.question);
+    
 }
-
+//Countdown to show how many seconds remain
+function questionTimer(){
+    setInterval(function(){
+        time--;
+        $("#time-remaining").text(time);
+    }, 1000);
+};
+//Multiple choice options displayed
 function showAnswers(){
     var correctAnswer = options.question1.answers[0];
     console.log(correctAnswer);
@@ -44,10 +52,15 @@ function showAnswers(){
       });
 
     for (var i = 0; i < multipleOptions.length; i++){
-        $("#answers").append("<p>" + multipleOptions[i] + "</p>");
+        $("#answers").append("<p class='answer-option'>" + multipleOptions[i] + "</p>");
     }
 
 }
+
+//User selects one option
+//Correct answer is shown. 
+    //User is told if they answered correctly or not
+    //# correct or incorrect answers increases
 
 });
 
